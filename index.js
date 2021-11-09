377e93656b4cb0e80ade6a8ac5fce0d84b639a36
Cameron Smith <cameron61624@mastek.com>
2021-11-09 16:34:48 +0000

    Added some practise

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("from external js file");
+const myCity = {
+    city: "Northampton",
+    popular: false,
+    country: "England"
+};
+
+let myObject;
+
+myObject={
+    a:10,
+    b:"abc"
+};
+
+myObject.c="Added";
+
+delete myObject.a;
+
+let myPost = {};
+
+myPost.postTitle = "Object is reference type";
+myPost.postLikes = 0;
+myPost.shared = false;
+
+myPost.postLikes +=1;
+
+delete myPost.shared;
